30b3b26ba49c6a0ae33322bbad3a21174442414c
Jack <jmcgregor@coveo.com>
2020-12-12 14:28:34 -0500

    adding GA - attempt 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
                 // The property ID; the tracking code won't be generated without it - could be G-078RRXMVEY
-                trackingId: 'G-256047902',
+                trackingId: 'G-078RRXMVEY',
                 // Defines where to place the tracking script - `true` in the head and `false` in the body
                 head: true,
                 // Setting this parameter is optional
